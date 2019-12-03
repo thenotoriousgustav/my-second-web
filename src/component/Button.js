4f47d0bfd02b7e1dfd6f021dea2e2bb9efec4998
Risti.nurfatiah 49 <fatiah2509@yahoo.com>
2019-12-03 09:45:18 +0700

    update version 2

---
+++ src/component/Button.js
@@ -0,0 +1,16 @@
+import React, { Components } from 'react';
+
+const Button = props => {
+    return(
+        <div style={styles.button}>
+     {props.title}
+      </div>
+    )
+}
+
+export default Button
+const styles ={
+    button:{
+        display:'flex', backgroundColor: 'orange', padding: 16, paddingLeft: 40, paddingRight: 40, color: '#fff', borderRadius: 10, marginLeft: 10
+        }
+}
